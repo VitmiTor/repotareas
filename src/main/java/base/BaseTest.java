486402c490d2f21bc099c807a2f716e334ae2191
VitmiTor <torremvictor@gmail.com>
2022-08-27 18:58:55 -0500

    cambiando utilities

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,10 +1,10 @@
 package base;
 
-import Utilities.Logs;
 import com.github.javafaker.Faker;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
+import utilities.Logs;
 
 public class BaseTest {
     protected final Logs logs = new Logs();
